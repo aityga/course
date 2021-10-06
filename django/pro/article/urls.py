8ef85e4393cc2d054d71890d3eea959ba8419bf5
aitegin <sealaa3@gmail.com>
2021-10-06 20:31:11 +0600

    ,,

--- django/pro/article/urls.py
+++ django/pro/article/urls.py
@@ -4,5 +4,8 @@ from .views import *
 urlpatterns = [
     path('', index),
     path('2', index2),
-    path('3', index3)
+    path('3', index3),
+    # path('4', index4),
+    # path('5', index5),
+    # path('6', index6),
 ]
--- django/pro/article/views.py
+++ django/pro/article/views.py
@@ -4,13 +4,28 @@ from django.http import HttpResponse
 # Create your views here.
 
 
-def index(requests):
-    return HttpResponse("Hello world")
+def index(request):
+    context = {
+        'mood': ['klass', 'norm', 'ujas']
+    }
+    return render(request, 'articles/index.html', context=context)
 
 
-def index2(requests):
-    return HttpResponse("Hello world2")
+def index2(request):
+    return render(request, 'articles/index2.html')
+#
+#
+def index3(request):
+    return HttpResponse('hi')
 
 
-def index3(requests):
-    return HttpResponse("Hello world3")
+# def index4(request):
+#     return render(request, 'articles/index4.html')
+#
+#
+# def index5(request):
+#     return render(request, 'articles/index5.html')
+#
+#
+# def index6(request):
+#     return render(request, 'articles/index6.html')
